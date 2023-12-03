5dbf3c063aaa4589ffeaa58dbc7d3bba0f8abbff
Satyansh Sharma <satyanshbu@gmail.com>
2023-12-02 23:14:32 -0800

    TycoonR updated + Backend Done

--- src/main/java/com/TycoonSim/Tycoon/TycoonRepository.java
+++ src/main/java/com/TycoonSim/Tycoon/TycoonRepository.java
@@ -1,12 +1,20 @@
 package com.TycoonSim.Tycoon;
 
-
 import com.google.cloud.spring.data.datastore.repository.DatastoreRepository;
 
 import java.util.List;
 
-public interface TycoonRepository extends DatastoreRepository<Tycoon, Long>  {
+/**
+ * Repository interface for interacting with Tycoon entities in Cloud Datastore.
+ */
+public interface TycoonRepository extends DatastoreRepository<Tycoon, Long> {
 
+    /**
+     * Finds Tycoon records by their score.
+     *
+     * @param score the score to search for
+     * @return a list of Tycoon records with the specified score
+     */
     List<Tycoon> findByScore(int score);
     //List<GameRecord> findByScore(int score);
 
