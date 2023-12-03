6681fff37b3b7808fb6f8e5179d4118dddf19a64
Satyansh Sharma <satyanshbu@gmail.com>
2023-12-02 23:13:44 -0800

    TycoonC updated

--- src/main/java/com/TycoonSim/Tycoon/TycoonController.java
+++ src/main/java/com/TycoonSim/Tycoon/TycoonController.java
@@ -1,6 +1,5 @@
 package com.TycoonSim.Tycoon;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,24 +10,44 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Controller class for handling Tycoon-related requests.
+ */
 @RestController
 public class TycoonController {
+
     private final TycoonRepository tycoonRepository;
 
+    /**
+     * Constructor for TycoonController.
+     *
+     * @param tycoonRepository the repository for Tycoon entities
+     */
     public TycoonController(TycoonRepository tycoonRepository) {
         this.tycoonRepository = tycoonRepository;
     }
 
+    /**
+     * Handles the POST request to save a TycoonRecord.
+     *
+     * @param tycoon the Tycoon object to be saved
+     * @return a string indicating the result of the save operation
+     */
     @PostMapping("/saveTycoonRecord")
     @CrossOrigin(origins = "*")
     public String saveTycoonRecord(@RequestBody Tycoon tycoon) {
         if (tycoon == null) {
-            return "The book is invalid";
+            return "The Tycoon record is invalid";
         }
         this.tycoonRepository.save(tycoon);
         return "success";
     }
 
+    /**
+     * Handles the GET request to find all TycoonRecords.
+     *
+     * @return a list of Tycoon objects representing all TycoonRecords
+     */
     @GetMapping("/findAllTycoonRecord")
     @ResponseBody
     @CrossOrigin(origins = "*")
@@ -38,6 +57,4 @@ public class TycoonController {
         tycoonRecords.forEach(tycoonList::add);
         return tycoonList;
     }
-
 }
-
